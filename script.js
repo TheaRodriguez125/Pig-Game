84fee4daaadcb3468fd39d36f5928ef51618465b
Thea Rodriguez <thearodriguez999@gmail.com>
2026-02-02 01:09:20 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,108 @@
+'use strict';
+
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const diceHolderEl = document.querySelector('.dice-holder');
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  playing = true;
+  activePlayer = 0;
+  btnRoll.disabled = false;
+  btnHold.disabled = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  diceHolderEl.classList.remove('hidden');
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+
+  current0El.textContent = '+0';
+};
+
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = '+0';
+
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+
+  btnHold.disabled = true;
+};
+
+btnRoll.addEventListener('click', function () {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    diceEl.src = `images/dice-${dice}.svg`;
+
+    diceHolderEl.classList.add('hidden');
+    diceEl.classList.remove('hidden');
+    btnHold.disabled = false;
+
+    if (dice !== 1) {
+      currentScore += dice;
+
+      document.getElementById(`current--${activePlayer}`).textContent =
+        `+${currentScore}`;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+
+      btnRoll.disabled = true;
+      btnHold.disabled = true;
+
+      playing = false;
+
+      document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+      diceHolderEl.classList.remove('hidden');
+      diceEl.classList.add('hidden');
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnNew.addEventListener('click', init);
